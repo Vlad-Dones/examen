fb67d339a71e940ee5e82596bf3dc07f4b142911
Vlad-Dones <vlad-donets.don@mail.ru>
2023-03-13 18:15:44 +0300

    Change tests 2

--- test/test_exam.py
+++ test/test_exam.py
@@ -5,24 +5,19 @@ import requests
 class TestAPI(unittest.TestCase):
 
     def test1(self):
-        resp = requests.get("http://127.0.0.1:5000/")
+        resp = requests.get("http://10.0.2.15:5000/")
         self.assertEqual(resp.status_code, 200)
         print("test1: OK")
 
     def test2(self):
-        resp = requests.get("http://127.0.0.1:5000/class")
+        resp = requests.get("http://10.0.2.15:5000/class")
         self.assertEqual(resp.status_code, 200)
         print("test2: OK")
 
     def test3(self):
-        resp = requests.post("http://127.0.0.1:5000/login")
+        resp = requests.post("http://10.0.2.15:5000/login")
         self.assertEqual(resp.status_code, 200)
         print("test3: OK")
-
-    def test4(self):
-        resp = requests.post("http://127.0.0.1:5000/login")
-        self.assertEqual(resp.status_code, 200)
-        print("test4: OK")
 
 
 if __name__ == '__main__':
@@ -30,4 +25,3 @@ if __name__ == '__main__':
     tester.test1()
     tester.test2()
     tester.test3()
-    tester.test4()
